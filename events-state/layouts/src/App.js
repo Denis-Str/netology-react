80b76309f8eea7538df2e83a9e8190b2a788c207
DenSTR <web_life@inbox.ru>
2023-02-09 19:50:42 +0300

    Расположение товаров

---
+++ events-state/layouts/src/App.js
@@ -0,0 +1,10 @@
+import React  from 'react';
+import Store from "./components/Store";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Store />
+    );
+  }
+}
