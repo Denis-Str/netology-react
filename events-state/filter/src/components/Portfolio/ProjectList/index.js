a9daa5d118b1248c4107d2d1612ded2cd2d64edb
DenSTR <web_life@inbox.ru>
2023-02-09 18:04:47 +0300

    Портфолио с фильтрами

---
+++ events-state/filter/src/components/Portfolio/ProjectList/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default class ProjectList extends React.Component {
+  render() {
+    const Image = ({project: {img, category}}) => <img src={img} alt={category}/>;
+    // const Image = ({project: {img, category}}) => <div>{category}</div>;
+
+    const projects = this.props.projects.map((project, index) =>
+      <li className='image' key={index}>
+        <Image project={project}/>
+      </li>);
+
+    return (
+      <ul className='project-list-component'>{projects}</ul>
+    )
+  }
+}
