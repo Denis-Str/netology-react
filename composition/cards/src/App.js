3e0eb41ba844a1fd3a10be3eaf984f3785d0b1b9
Denis <web_life@inbox.ru>
2023-02-15 20:11:16 +0300

    composition

---
+++ composition/cards/src/App.js
@@ -0,0 +1,20 @@
+import Card from "./common/Card";
+const cards = [
+  {src: 'https://via.placeholder.com/200'},
+  {src: ''},
+  {src: 'https://via.placeholder.com/200'},
+  {src: ''},
+  {src: 'https://via.placeholder.com/200'},
+]
+
+export default function App() {
+  const cardsList = cards.map(({src}, index) => {
+    if (src) {
+      const viewImg = <img src={src} className="card-img-top" alt="picture 1" />
+      return (<Card key={index}>{viewImg}</Card>)
+    } else return <Card key={index}/>
+  })
+  return (
+    <div className="cards">{cardsList}</div>
+  );
+}
