3bbcdc5ed64ec2c4f604498705da36ab5b944e0b
Denis <web_life@inbox.ru>
2023-03-04 15:08:55 +0300

    hoc

--- hoc/highlight/src/hoc/Wrapper.jsx
+++ hoc/highlight/src/hoc/Wrapper.jsx
@@ -1,11 +1,14 @@
+import React from "react";
 import Popular from "../components/Popular";
 import New from "../components/New"
 
- const Wrapper = ({children}) => {
-  const {views} = children.props
-  if (views > 1000) return <Popular>{children}</Popular>;
-  if (views < 100) return <New>{children}</New>;
-  return <>{children}</>;
+export default function Wrapper(Component, item) {
+  const { views } = item;
+  return class extends React.Component {
+    render() {
+      if (views < 100) return (<New><Component {...item} /></New>);
+      if (views > 1000) return (<Popular><Component {...item} /></Popular>);
+      return (<Component {...item} />);
+    }
+  }
 }
-
-export default Wrapper;
