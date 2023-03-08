4914af7c6724d00a267b9c0207af7dd7268b11ef
DenSTR <web_life@inbox.ru>
2023-03-08 12:06:33 +0300

    menu

--- router/menu/src/App.js
+++ router/menu/src/App.js
@@ -1,25 +1,23 @@
 import {Route, Routes} from "react-router-dom";
-import { BrowserRouter } from 'react-router-dom';
-import HomePage from "./components/HomePage";
-import DriftPage from "./components/DriftPage";
-import ForzaPage from "./components/ForzaPage";
-import TimeAttackPage from "./components/TimeAttackPage";
+import HomePage from "./pages/HomePage";
+import DriftPage from "./pages/DriftPage";
+import ForzaPage from "./pages/ForzaPage";
+import TimeAttackPage from "./pages/TimeAttackPage";
 import Menu from "./components/Menu";
+import React from "react";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Menu />
-        <div className="page">
-          <Routes location>
-            <Route path="/" exact element={HomePage} />
-            <Route path="/drift" element={DriftPage} />
-            <Route path="/timeattack" element={TimeAttackPage} />
-            <Route path="/forza" element={ForzaPage} />
-          </Routes>
-        </div>
+    <div className="app">
+      <Menu/>
+      <div className="page">
+        <Routes>
+          <Route path="/" exact element={<HomePage/>}/>
+          <Route path="drift" element={<DriftPage/>}/>
+          <Route path="timeattack" element={<TimeAttackPage/>}/>
+          <Route path="forza" element={<ForzaPage/>}/>
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
