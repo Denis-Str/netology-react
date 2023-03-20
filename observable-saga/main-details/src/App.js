cdf1070f451f51a7be2683bbad89822d3a5d6fbf
DenSTR <web_life@inbox.ru>
2023-03-20 16:18:14 +0300

    redux async init

---
+++ observable-saga/main-details/src/App.js
@@ -0,0 +1,10 @@
+// import { useSelector, useDispatch } from "react-redux";
+// import { preview, list, addImg, addPreview } from "./redux/gallery";
+
+export default function App() {
+  return (
+    <div className="wrap">
+      dfsfdsd
+    </div>);
+}
+
---
+++ observable-saga/search/src/redux/index.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import galleryReducer from "./gallery";
+
+export default configureStore({
+  reducer: {
+  },
+})
