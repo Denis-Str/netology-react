b407df2fddf7cfe427326c1b5b1d9a1ca449416c
DenSTR <web_life@inbox.ru>
2023-03-22 16:17:40 +0300

    redux async init

--- observable-saga/search/src/App.js
+++ observable-saga/search/src/App.js
@@ -1,22 +1,28 @@
 import { useSelector, useDispatch } from "react-redux";
 import { fetchResult, result, isLoading, error } from "./redux/search";
+import debounce from "./helpers/debounce";
 
 export default function App() {
   const resultList = useSelector(result) || [];
   const errorMessage = useSelector(error) || '';
   const loading = useSelector(isLoading) || false;
   const dispatch = useDispatch();
-  const renderList = <ul>{resultList.map(({id, name}) => <li key={id}>{name}</li>)}</ul>;
+
+  const handlerChange = debounce(value => {
+    dispatch(fetchResult(value));
+  }, 300)
+
+  const listElem = <ul>{resultList.map(({id, name}) => <li key={id}>{name}</li>)}</ul>;
   const loadingElem = <div className="loading">{loading && "loading..."}</div>;
   const errorElem = errorMessage && <h5>{errorMessage}</h5>;
 
   let searchResultElem;
-  if (resultList.length) searchResultElem = renderList
+  if (resultList.length) searchResultElem = listElem
   else searchResultElem =<ul><li>not results</li></ul>
 
   return (
     <>
-      <input onChange={(event) => dispatch(fetchResult(event.target.value))} />
+      <input onChange={(event) => handlerChange(event.target.value)} />
       {errorMessage ? errorElem : loading ? loadingElem : searchResultElem}
     </>
   )
